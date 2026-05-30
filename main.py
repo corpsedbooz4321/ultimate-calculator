d70ccdb63894d2c5330c24ff1c68722dc95c6d32
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-05-30 09:39:29 +0000

    Fix simple interest calculator - add error handling and menu loop

--- main.py
+++ main.py
@@ -2,31 +2,42 @@
 
 print("=============THE SIMPLE INTEREST CALCULATOR=============")
 
-def calculate():
-    P = float(input("\nEnter your Principle: "))
-    R = float(input("\nEnter your rate: "))
-    T = float(input("\nEnter your time: "))
-    
+
+def calculate():#SIMPLE INTEREST block.
+    while True:   
+        try:
+           P = float(input("\nEnter your Principal: "))
+           R = float(input("\nEnter your rate: "))
+           T = float(input("\nEnter your time: "))
+           break
+        except ValueError:
+            print("\nInvalid input...please enter a number...")
+            continue
+
     interest = (P*T*R)/100
     total_amount = P + interest
 
     print("\n========RESULTS========")
     print(f"Calculated interest: {interest}")    
     print(f"Total amount: {total_amount}")
+    
+    input("\nPress Enter to return to MENU....")
 
-def menu():
+def menu():#MENU block.
+    print("\nMENU")
     print("\n=============select what you want to perform============")
-    print("\n1. To calculate simple interest")
+    print("\n1. To calculate simple interest")#calulates the interest form the users data.
     print("\n2. To exit")
     
     while True:
-        action = int(input("\nEnter what you want to perform: "))
-
-        if action == 1:
+        try:
+            action=int(input("\nEnter what you want to perform: "))
+        except ValueError:
+            print("\nInvalid input...please enter a number! ")
+            continue
+        if action==1:
             calculate()
-        elif action ==2:
-            exit()
-        else:
-            print("invalid input")
+        elif action==2:
+            print("Exiting....")
+            break
 menu()
-
