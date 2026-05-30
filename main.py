ce9d8d520754855258a93135b84191fd3b52f001
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-05-30 07:23:37 +0000

    added menu option

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #Simple interest calculator.
+
 print("=============THE SIMPLE INTEREST CALCULATOR=============")
-P = float(input("Enter your interest: "))
-R = float(input("Enter your rate: "))
-T = float(input("Enter your time: "))
-def caculate():
+
+def calculate():
+    P = float(input("\nEnter your Principle: "))
+    R = float(input("\nEnter your rate: "))
+    T = float(input("\nEnter your time: "))
+    
     interest = (P*T*R)/100
     total_amount = P + interest
 
@@ -15,7 +18,15 @@ def menu():
     print("\n=============select what you want to perform")
     print("\n1. To calculate simple interest")
     print("\n2. To exit")
+    
+    while True:
+        action = int(input("\nEnter what you want to perform: "))
 
-if action = 
-
+        if action == 1:
+            calculate()
+        elif action ==2:
+            exit()
+        else:
+            print("invalid input")
+menu()
 
